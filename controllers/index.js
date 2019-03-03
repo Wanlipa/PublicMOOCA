eb2c29488cb34ddadfa2dc73e6906fd9c30fa9ad
wanlipa <nuam18@gmail.com>
2019-03-03 13:14:46 +0700

    Relationship Result and Import Page

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,7 @@
 exports.getIndex = (req, res, next) => {
     res.render('index', {pageTitle: 'Main',path: '/'});
 };
+
+exports.getImport = (req, res, next) => {
+    res.render('import', {pageTitle: 'Import', path: '/import'});
+};
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const indexController = require('../controllers/index');
 
 router.get('/', indexController.getIndex);
+router.get('/import', indexController.getImport);
 
 module.exports = router;
